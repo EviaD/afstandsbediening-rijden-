fb36b8495af12ed4caa62e09b4fa29d2976f6e87
EviaD <108572417+EviaD@users.noreply.github.com>
2024-05-04 19:47:07 +0200

    regenboog toevoegen

--- main.ts
+++ main.ts
@@ -15,6 +15,10 @@ function IR_verwerken () {
         maqueenPlusV2.controlLED(maqueenPlusV2.MyEnumLed.AllLed, maqueenPlusV2.MyEnumSwitch.Open)
     } else if (message == 149) {
         maqueenPlusV2.controlLED(maqueenPlusV2.MyEnumLed.AllLed, maqueenPlusV2.MyEnumSwitch.Close)
+    } else if (message == 240) {
+        Regenboog_lichtjes()
+    } else if (message == 121) {
+        maqueenPlusV2.ledBlank()
     } else {
         basic.showString("" + (IR.IR_read()))
     }
@@ -22,13 +26,25 @@ function IR_verwerken () {
 input.onButtonPressed(Button.A, function () {
     maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.AllMotor)
 })
+function Regenboog_lichtjes () {
+    maqueenPlusV2.showColor(maqueenPlusV2.NeoPixelColors.Red)
+    basic.pause(100)
+    maqueenPlusV2.showColor(maqueenPlusV2.NeoPixelColors.Orange)
+    basic.pause(100)
+    maqueenPlusV2.showColor(maqueenPlusV2.NeoPixelColors.Yellow)
+    basic.pause(100)
+    maqueenPlusV2.showColor(maqueenPlusV2.NeoPixelColors.Green)
+    basic.pause(100)
+    maqueenPlusV2.showColor(maqueenPlusV2.NeoPixelColors.Blue)
+    basic.pause(100)
+    maqueenPlusV2.showColor(maqueenPlusV2.NeoPixelColors.Purple)
+    basic.pause(100)
+}
 let message = 0
 maqueenPlusV2.controlLED(maqueenPlusV2.MyEnumLed.AllLed, maqueenPlusV2.MyEnumSwitch.Close)
 basic.forever(function () {
     message = IR.IR_read()
     if (message != -1) {
         IR_verwerken()
-    } else {
-    	
     }
 })
